e1e054234f7a16d4237f0fb5fe5cca20cb26eb0b
Digital Artifex <7929434+DigitalArtifex@users.noreply.github.com>
2025-09-21 07:08:58 -0400

    Added Pexels and ShaderToy API

--- plugin/plugin.cpp
+++ plugin/plugin.cpp
@@ -5,6 +5,10 @@
 #include "AudioModel.h"
 #include "AudioImageProvider.h"
 #include "ShaderPackModel.h"
+#include "PexelsVideoSearch.h"
+#include "PexelsImageSearch.h"
+#include "ShaderToySearchModel.h"
+#include "GeometryProvider.h"
 #include "Komplex_global.h"
 
 AudioModel *komplexAudioSingletonProvider(QQmlEngine *engine, QJSEngine *scriptEngine)
@@ -27,13 +31,27 @@ public:
 	void registerTypes(const char *uri) override
     {
         Q_ASSERT(QLatin1String(uri) == QLatin1String("com.github.digitalartifex.komplex"));
+
+        char *stUri = new char[std::strlen(uri) + std::strlen(".ShaderToy")];
+        std::sprintf(stUri, "%s.ShaderToy", uri);
+
+        char *pvUri = new char[std::strlen(uri) + std::strlen(".Pexels.Video")];
+        std::sprintf(pvUri, "%s.Pexels.Video", uri);
+
+        char *piUri = new char[std::strlen(uri) + std::strlen(".Pexels.Image")];
+        std::sprintf(piUri, "%s.Pexels.Image", uri);
     
         qmlRegisterSingletonType<AudioModel*>(uri, 1, 0, "AudioModel", komplexAudioSingletonProvider);
         qmlRegisterType<ShaderPackModel>(uri, 1, 0, "ShaderPackModel");
+        qmlRegisterType<GeometryProvider>(uri, 1, 0, "GeometryProvider");
+        qmlRegisterType<ShaderToySearchModel>(stUri, 1, 0, "SearchModel");
+        qmlRegisterType<PexelsVideoSearchModel>(pvUri, 1, 0, "SearchModel");
+        qmlRegisterType<PexelsImageSearchModel>(piUri, 1, 0, "SearchModel");
     }
 
     void unregisterTypes() override
     {
+        AudioModel::stopCapture();
     }
 
     void initializeEngine(QQmlEngine *engine, const char *uri) override
